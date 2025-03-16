6d8bdb4034bd833ccf6adc6daf355ba33dd7e810
Thad House <thadhouse1@gmail.com>
2025-03-15 22:53:31 -0700

    Fix direction

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -55,7 +55,7 @@ public class RobotContainer {
             () ->
                 m_robotDrive.drive(
                     -m_driverController.getLeftY(),
-                    -m_driverController.getLeftX(),
+                    m_driverController.getLeftX(),
                     m_driverController.getRightX(),
                     false),
             m_robotDrive));
